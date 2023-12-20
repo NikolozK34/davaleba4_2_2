fa1c149cec3f580402f15155433bbeafbcc8c6aa
NikolozK34 <kekelidze97@gmail.com>
2023-12-20 13:59:44 +0400

    Add project files.

---
+++ davaleba4_2_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class CombineArraysExample
+{
+    static void Main()
+    {
+        int[] firstArray = { 1, 2, 3 };
+
+        int[] secondArray = { 4, 5, 6 };
+
+        int[] thirdArray = new int[firstArray.Length + secondArray.Length];
+
+        for (int i = 0; i < firstArray.Length; i++)
+        {
+            thirdArray[i] = firstArray[i];
+        }
+
+        for (int i = 0; i < secondArray.Length; i++)
+        {
+            thirdArray[secondArray.Length + i] = secondArray[i];
+        }
+
+        Console.WriteLine("Third Array:");
+        foreach (var item in thirdArray)
+        {
+            Console.Write($"{item} ");
+        }
+    }
+
+   
+}
